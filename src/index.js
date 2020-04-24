e91ec7e16c415e7dd85d2f7bde59229c03c71d9c
happyjy <okwoyjy@gmail.com>
2020-04-25 01:11:08 +0900

    #3 2 mapStateToProps

--- src/index.js
+++ src/index.js
@@ -13,6 +13,21 @@ ReactDOM.render(
 
 /**
  * 
- * 1. Provider(react-redux)에 생성한 "stroe"를 설정함으로 App에서 store를 사용할 수 있다. 
+ * 1. Provider(react-redux)에 생성한 "stroe"를 설정함으로 store가 변경될때 App에서 store를 사용할 수 있다. 
+ * 2. connect 
+ *  - component와 store를 연결해주는 react-redux 객체
  * 
+ */
+
+
+
+
+
+
+
+
+
+ /* 
+ To use connect(), you need to define a special function called mapStateToProps that describes how to transform the current Redux store state into the props you want to pass to a presentational component you are wrapping. 
+ 
  */
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,8 @@
 import React, { useState }from "react";
+import { connect } from "react-redux";
 
-function Home(){
+function Home({ toDos, abc }){
+  console.log("### Home component: ", toDos, abc)
   const [text, setText] = useState("");
   function onChange(e){
     setText(e.target.value);
@@ -8,6 +10,7 @@ function Home(){
 
   function onSubmit(e){
     e.preventDefault();
+    setText("");
     console.log("### onSubmit: ", text);
   }
   return (
@@ -16,11 +19,18 @@ function Home(){
       <form onSubmit={onSubmit}>
         <input type="input" value={text} onChange={onChange}></input>
         <button>Add</button>
-      </form>
-      <ul></ul>
+      </form> 
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   )
 }
 
-export default Home;
+//connect를 사용해서 Home으로 보내주는 props에 추가 될 수 있도록 허용 
+//mapStateToProps return에 보내주는 값은 Home props로 받을 수 있다. 
+function mapStateToProps(state, ownProps){
+  console.log("### mapStateToProps", {state, ownProps});
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
 
