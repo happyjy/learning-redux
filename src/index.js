dadab10824b5f923578470916e55f51ee27a44cf
happyjy <okwoyjy@gmail.com>
2020-04-24 22:10:50 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import { createStore } from "redux";
 //#1.1 createStore/ redecuer
 //  createStore(reducer).getState();
 
-//#1.2 Actions
-// dispatch
+//#1.2 Actions: store의 값을 변경
+// createStore(reducer).dispatch( {type: ""} );
+
+//#1.3 Subscriptions: dispatch 이후 동작
+// createStore(reducer).subscribe(function(){});
 
 //reducer
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
 const countModifier = (count = 0, action) => {
   console.log({count, action}); //eg) action = { type: "MINUS", count:1 }
   
@@ -25,8 +32,22 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+}
+
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
 
 console.log(countStore.getState());
