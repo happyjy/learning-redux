ffd68f625c1288753f2ae796c844cc0c89ba26db
happyjy <okwoyjy@gmail.com>
2020-04-24 21:59:38 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import { createStore } from "redux";
 
-// const add = document.getElementById("add");
-// const minus = document.getElementById("minus");
-// const number = document.querySelector("span");
+//#1.1 createStore/ redecuer
+//  createStore(reducer).getState();
 
-const countModifier = (count = 0) => {
-  return count;
+//#1.2 Actions
+// dispatch
+
+//reducer
+const countModifier = (count = 0, action) => {
+  console.log({count, action}); //eg) action = { type: "MINUS", count:1 }
+  
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+      break;
+    case "MINUS":
+      return count -1;
+      break;
+    default:
+      return count;
+      break;
+  }
 }
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
