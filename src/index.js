a0a011f3345035fb04fe2486eaaaa8110ec7d07e
happyjy <okwoyjy@gmail.com>
2020-04-25 00:49:17 +0900

    #3.1 Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from"react-dom";
 import App from "./component/App";
+import { Provider } from "react-redux";
+import store from "./store";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById("root")
+);
+
+/**
+ * 
+ * 1. Provider(react-redux)에 생성한 "stroe"를 설정함으로 App에서 store를 사용할 수 있다. 
+ * 
+ */
