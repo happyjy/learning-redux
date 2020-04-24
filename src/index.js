dc59d65c4ac58af88ba6dcf0266ab69b8d9a3b3b
happyjy <okwoyjy@gmail.com>
2020-04-24 22:53:07 +0900

    #2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -4,24 +4,47 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
+//
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
+//reducer
 const reducer = (state = [], action) => {
   console.log({state, action});
   switch (action.type) {
     case ADD_TODO:
       return [{ text: action.text, id: Date.now()}, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter( toDo => toDo.id !== action.id );
     default:
       return state;
   }
 }
 
+//store
 const store = createStore(reducer);
 
+const dispatchAddTodo = text => {
+  store.dispatch(addToDo(text));
+}
+
 const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
 }
 
 const paintTodos = () => {
@@ -48,6 +71,7 @@ const submit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  // store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddTodo(toDo);
 }
 form.addEventListener("submit", submit)
